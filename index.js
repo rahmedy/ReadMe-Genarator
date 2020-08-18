430a0c26b975507d5d8b0f093de57028c1b1c9a7
Rayan AHmedy <rayanahmedy1999@gmail.com>
2020-08-17 20:49:44 -0400

    questions to add for read me

--- index.js
+++ index.js
@@ -5,7 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // array of questions for user
 const questions = [
-
+    {
+        type: "input",
+        message: "Project Title:",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Application Description:",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Install Instructions:",
+        name: "install",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "Usage:",
+        name: "usage"
+    },
+    {
+        type: "list",
+        message: "License Used:",
+        name: "license",
+        choices: ["MIT", "APACHE", "GPL", "Affero GPL", "Artistic License 2.0", "Public Domain (unlicense)", "n/a"]
+    },
+    {
+        type: "input",
+        message: "Contributing:",
+        name: "contributing"
+    },
+    {
+        type: "input",
+        message: "Testing Instructions:",
+        name: "test",
+        default: "run 'npm test'"
+    },
+    {
+        type: "input",
+        message: "GitHub Username:",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Email Address:",
+        name: "email"
+    }
 ];
 
 // function to write README file
